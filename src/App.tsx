ef861edd73d867b4822a6db60d8033fe377308da
Ki Hyun Park <kihyun1998@naver.com>
2026-03-29 19:16:42 +0900

    init

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <h1 className="text-4xl font-bold">Hello, Tauri + React</h1>
+    </div>
+  );
+}
+
+export default App;
